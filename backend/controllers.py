4427d78d3577c97fb6f47af05f104d98f243abe9
meghamondal <19meghamondal@gmail.com>
2025-03-02 22:57:27 +0530

    worked on controllers and admin_dashboard

--- backend/controllers.py
+++ backend/controllers.py
@@ -22,15 +22,33 @@ def login():
     else:
       return render_template("login.html", msg="Invalid credentials...")
     
-@app.route('/admin_dashboard', methods=["GET","POST"])
+'''@app.route('/admin_dashboard', methods=["GET","POST"])
 def admin_dashboard():
   if request.method=="GET":
-    return render_template("admin_dashboard.html")
+    return render_template("admin_dashboard.html")'''
   
-  @app.route("/register",method=['GET','POST'])
-  def register():
-    if request.method=='GET':
-      return render_template('register.html')
-    
+@app.route("/register", methods=['GET','POST'])
+def register():
+  if request.method=='POST':
+    email=request.form.get("email")
+    password=request.form.get("password")
+    f_name=request.form.get("f_name")
+    l_name=request.form.get("l_name")
+    qualification=request.form.get("qualification")
+    dob=request.form.get("dob")
+    if(dob):
+      dob=datetime.strptime(dob, '%Y-%m-%d')
+    user=Authentication.query.filter_by(email=email).first()
+    if user: 
+      return render_template("register.html", msg="Sorry, this email is already registered!")
+    new_user_auth=Authentication(email=email, password=password, role='user')
+    new_user=User(email=email,f_name=f_name, l_name=l_name, qualification=qualification, dob=dob)
 
-    
+    db.session.add(new_user_auth)
+    db.session.add(new_user)
+    db.session.commit()
+    return render_template("login.html", msg="Registration successfull, please proceed to login now!")
+  return render_template('register.html', msg="")
+
+'''@app.route("/admin_dashboard", methods=['GET','POST'])  
+def admin_dashboard():'''
